b3ce32439e81cee6d9c3ea861ae057cc48f7e94f
Javi Martinez <javier.martinez.snt@gmail.com>
2024-11-05 17:47:09 +0100

    add connect to db

---
+++ db/database.go
@@ -0,0 +1,33 @@
+package database
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2/log"
+	"github.com/javimartzs/pill-reminder-app-v0/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func InitDB(config *config.EnvConfig) *gorm.DB {
+
+	uri := fmt.Sprintf("host=%s user=%s dbname=%s password=%s port=%s",
+		config.DBHost, config.DBUser, config.DBName, config.DBPass, config.DBPort)
+
+	db, err := gorm.Open(postgres.Open(uri), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		log.Fatalf("Unable to connect to database %v", err)
+	}
+
+	log.Info("Connected to the database")
+
+	if err := DBMigrator(db); err != nil {
+		log.Fatalf("Unable to migrate: %v", err)
+	}
+
+	return db
+}
